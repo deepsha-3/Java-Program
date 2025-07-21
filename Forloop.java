8d4b9d48db08ba8dbf36c1b62c26b85882b1633d
deepsha-3 <itsmedeepsha375@gmail.com>
2025-07-21 23:01:07 +0545

    new files

---
+++ Forloop.java
@@ -0,0 +1,10 @@
+// This Java program uses a for loop to print numbers from 1 to 5.
+public class Forloop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Iteration " + i);
+        }
+    }
+}
+
+
---
+++ If_else_if.java
@@ -0,0 +1,16 @@
+    // This Java program checks if a number is positive, negative, or zero using if-else-if statements.
+public class If_else_if {
+    public static void main(String[] args) {
+        int num = 5;
+        
+        if (num > 0) {
+            System.out.println("Number is Positive");
+        } else if (num < 0) {
+            System.out.println("Number is Negative");
+        } else {
+            System.out.println("Number is Zero");
+        }
+    }
+}
+
+
