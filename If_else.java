1dafa4e8d6f8d1bc94965a965f03a47c3d211d41
deepsha-3 <itsmedeepsha375@gmail.com>
2025-07-21 21:25:46 +0545

    first program

---
+++ If_else.java
@@ -0,0 +1,13 @@
+// This Java program checks if the sum of two integers is less than 20 and uses an if-else statement to check the condition.
+public class If_else {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 12;
+
+        if (x + y < 20) {
+            System.out.println("x + y is less than 20");
+        } else {
+            System.out.println("x + y is greater than 20");
+        }
+    }
+}
