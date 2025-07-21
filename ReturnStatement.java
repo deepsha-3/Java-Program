e00542cd2309f5ec4f901a07c345de19d533d177
deepsha-3 <itsmedeepsha375@gmail.com>
2025-07-21 23:30:07 +0545

    new file added

---
+++ ReturnStatement.java
@@ -0,0 +1,13 @@
+// This Java program demonstrates the use of a return statement in a method.
+public class ReturnStatement {
+
+    public static void main(String[] args) {
+        System.out.println("Result: " + getvalue());
+    }
+
+    public static int getvalue() {
+        return 10;
+    }
+}
+
+
