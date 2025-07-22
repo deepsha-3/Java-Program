b52fabda5036a2623a2d3c3af852c96fac544f20
deepsha-3 <itsmedeepsha375@gmail.com>
2025-07-22 22:45:07 +0545

    new file added

---
+++ DefaultConstructor.java
@@ -0,0 +1,14 @@
+// This Java program demonstrates the use of a default constructor.
+         // It shows how a default constructor initializes instance variables.
+public class DefaultConstructor {
+ int a;
+                 // Default constructor
+ public DefaultConstructor() {
+ a =1; }
+public static void main(String[] args) {
+ DefaultConstructor obj = new DefaultConstructor();
+ System.out.println("Value of a:"+obj.a);
+ }
+}
+
+
