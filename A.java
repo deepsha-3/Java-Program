9f033e28304d7ab6e9dcc4392a358eff7dcb5358
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-03 22:54:03 +0545

    new file

---
+++ A.java
@@ -0,0 +1,22 @@
+
+public class A {
+    int a;
+    int b;
+
+    // Default constructor
+    public A() {
+        this(2, 3); // Call parameterized constructor
+    }
+
+    // Parameterized constructor
+    public A(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        A obj = new A();
+        System.out.println("a = " + obj.a + ", b = " + obj.b);
+    }
+}
