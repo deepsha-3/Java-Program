9ce756b008c4075786f895d737d138f4689b71a7
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-03 22:58:54 +0545

    new file

---
+++ ArithmeticExample.java
@@ -0,0 +1,16 @@
+
+
+public class ArithmeticExample {
+    public static void main(String[] args) {
+        try {
+            int a = 5;
+            int b = 0;
+            int result = a / b;  // This will cause ArithmeticException
+            System.out.println("Print Result: " + result);
+        } catch (ArithmeticException e) { // Catch must include exception variable
+            System.out.println("Exception: " + e.getMessage());
+        } finally {
+            System.out.println("This is finally finished");
+        }
+    }
+}
